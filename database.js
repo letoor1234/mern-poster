80306745fcec4a9dde1d562e1073ade350691588
angelito <angelmartinezwb@gmail.com>
2021-03-03 02:25:16 -0300

    mlab

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose');
 
-const URI = 'mongodb://localhost/tasker';
+const URI = 'mongodb+srv://letoor1234:1475369@mern-poster-db.getjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(db =>{
