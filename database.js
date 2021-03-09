9de1eb4c1a78680475c63bac2228ab7564302416
angelito <angelmartinezwb@gmail.com>
2021-03-09 02:41:51 -0300

    add enviroment var fordatabase uri!

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose');
 
-const URI = 'mongodb+srv://letoor1234:1475369@mern-poster-db.getjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const URI = process.env.MLAB_URI;
 
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(db =>{
